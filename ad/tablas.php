6264c5b2bc33626c6f3fe34b1de8c9c08e8ed18d
Jtom22 <90915630+Jtom22@users.noreply.github.com>
2023-06-06 13:35:13 +0200

    web
    Muestra los datos

--- ad/tablas.php
+++ ad/tablas.php
@@ -3,6 +3,7 @@
     $usuario="root";
     $clave="";
     $baseDeDatos="elpalacio";
+    
 
     $enlace = mysqli_connect($servidor,$usuario,$clave, $baseDeDatos);
     if(!$enlace){
@@ -14,30 +15,38 @@
 <HTML>
   <head>
     <title>Mi primer WEB</title>
-    <link  href="style2.css" rel="stylesheet" type="text/css" >
+    <link  href="tablaCss.css" rel="stylesheet" type="text/css" >
+    
   </head>
   <body>
   
     <?php 
-        $consulta ="SELECT * from reservas";
+        
         $usuario=$_POST['username'];
         echo $usuario;
-        $query = $mysqli->query("SELECT * from reservas where usuario='$usuario'");
-        if ($result = $mysqli->query($query)) {
-
-            /* fetch associative array */
-            while ($row = $result->fetch_assoc()) {
-                $field1name = $row["col1"];
-                $field2name = $row["col2"];
-                $field3name = $row["col3"];
-                $field4name = $row["col4"];
-                $field5name = $row["col5"];
-            }
+        $consulta ="SELECT * from reservas where id_cliente='$usuario'";
         
-            /* free result set */
-            $result->free();
+
+       
+        //$resultado=$mysqli_query($enlace,$consulta);
+        //$res=$mysqli_query();
+        $res = $enlace->query($consulta);
+     
+        if ($res->num_rows > 0) {
+          echo "<table id=tablas><thead><tr><th>ID</th><th>Nombre</th><th>Correo</th><th>Telefono</th><th>Fecha</th><th>Estado</th><th>Estado</th></tr></thead>";
+          // output data of each row
+          while($row = $res->fetch_assoc()) {
+            echo "<tbody><tr><td>".$row["id"]."</td><td>".$row["nombre"]."<td>".$row["correo"]."</td><td>".$row["telefono"]."</td><td>".$row["fecha"]."</td><td>".$row["Estado"]."</td></tr></tbody>";
+             
+          }
+          echo "</table>";
+        } else {
+          echo "0 results";
         }
+        $enlace->close();
   
     ?>
+ 
+
   </body> 
 </HTML>
